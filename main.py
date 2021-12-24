7dfdd77c4f88380dd78ec0777345e51b3388b014
ManHinnn0509 <79153615+ManHinnn0509@users.noreply.github.com>
2021-12-25 06:00:06 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import *
+
+from pytube import YouTube
+
+class YTDL_Window:
+    def __init__(self):
+        self.master = tk.Tk()
+
+    def start(self):
+        self.master.mainloop()
+
+def main():
+    pass
+
+if (__name__ == "__main__"):
+    main()
